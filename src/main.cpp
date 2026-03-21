8c659622212d974a0d4e9779c7db0a14683f4168
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-20 21:23:44 -0700

    Use custom address and shorten init code

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,7 @@
 #define RF24_CE     5
 
 RF24 radio(RF24_CE, SPI0_CSN);
-uint8_t address[][6] = { "1Node", "2Node" };
-
-bool radioNumber = 0;  // 0 uses address[0] to transmit, 1 uses address[1] to transmit
+uint8_t address[5] = { 0xCE, 0x15, 0x10, 0x55, 0xBB };
 
 float payload = 0.0;
 
@@ -26,26 +24,14 @@ void setup() {
   SPI.setSCK(SPI0_SCK);
   SPI.setMOSI(SPI0_MOSI);
 
-  while (!Serial2) {
-    // some boards need to wait to ensure access to serial over USB
-  }
-
-  // initialize the transceiver on the SPI bus
+  while (!Serial2);
   if (!radio.begin()) {
     Serial2.println(F("radio hardware is not responding!!"));
-    while (1) {}  // hold in infinite loop
+    while (1);
   }
 
-  radioNumber = 0;
-  Serial2.print(F("radioNumber = "));
-  Serial2.println((int)radioNumber);
   radio.setPayloadSize(sizeof(payload));  // float datatype occupies 4 bytes
-
-  // set the TX address of the RX node for use on the TX pipe (pipe 0)
-  radio.stopListening(address[radioNumber]);  // put radio in TX mode
-
-  // set the RX address of the TX node into a RX pipe
-  radio.openReadingPipe(1, address[!radioNumber]);  // using pipe 1
+  radio.stopListening(address);
 }
 
 void loop() {
