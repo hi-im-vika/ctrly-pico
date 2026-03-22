22c7073e61c02f9b11ffcfbdac686d0005f76140
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-21 17:05:40 -0700

    Nonblocking write InputFrame to radio and remove example payload

--- src/main.cpp
+++ src/main.cpp
@@ -25,7 +25,6 @@ InputFrame input = {};
 RF24 radio(RF24_CE, SPI0_CSN);
 uint8_t address[5] = { 0xCE, 0x15, 0x10, 0x55, 0xBB };
 
-float payload = 0.0;
 unsigned long last_report = millis();
 
 /*---Count Variables---*/ 
@@ -69,7 +68,7 @@ void setup() {
   Serial2.printf("TX address: 0x%X 0x%X 0x%X 0x%X 0x%X\r\n",
     address[0], address[1], address[2], address[3], address[4]);
   radio.setPALevel(RF24_PA_LOW);
-  radio.setPayloadSize(sizeof(payload));  // float datatype occupies 4 bytes
+  radio.setPayloadSize(FRAME_SIZE);  // float datatype occupies 4 bytes
   radio.setAutoAck(false);
   radio.setRetries(0,0);
   radio.setDataRate(RF24_2MBPS);
@@ -80,10 +79,15 @@ void setup() {
 
 void loop() {
   read_serial();
-  bool report = radio.write(&payload, sizeof(float));  // transmit & save the report
-  if (report) payload += 0.01;
+  bool report = radio.writeFast(&input, FRAME_SIZE);  // transmit & save the report
+  if (!radio.txStandBy(1000)) {
+    radio.flush_tx();
+    Serial2.println("TX FAIL");
+  } else {
+    tx_count++;
+  }
   if (millis() - last_report > 10) {
     last_report = millis();
-    Serial2.printf("RX: %d\r", rx_count);
+    Serial2.printf("RX: %d TX: %d\r", rx_count, tx_count);
   }
 }
