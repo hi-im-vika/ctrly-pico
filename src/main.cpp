614892b1049c87aca9c582d493dabe5a4f90451f
vika <37137412+hi-im-vika@users.noreply.github.com>
2026-03-23 00:45:59 -0700

    Fix fn name

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ unsigned long fail_count = 0;
 	@return Encoded buffer length in bytes
 	@note Does not output delimiter byte
 */
-size_t cobsEncode(const void *data, size_t length, uint8_t *buffer) {
+size_t cobs_encode(const void *data, size_t length, uint8_t *buffer) {
 	assert(data && buffer);
 
 	uint8_t *encode = buffer; // Encoded byte pointer
